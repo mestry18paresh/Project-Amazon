26be0307b979c2872ba0f1e2d3c4cf826baa6927
Paresh <mestry18paresh@gmail.com>
2024-09-08 12:26:33 +0530

    CICD Update in POM

--- src/test/java/com/amazon/MultiBrowserTest.java
+++ src/test/java/com/amazon/MultiBrowserTest.java
@@ -1,10 +1,16 @@
 package com.amazon;
 
+import java.io.File;
+import java.io.IOException;
+
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Parameters;
@@ -12,41 +18,45 @@ import org.testng.annotations.Test;
 
 public class MultiBrowserTest {
 
-    private WebDriver driver;
+	private WebDriver driver;
+	String bName;
 
-    @BeforeClass
-    @Parameters("browser")
-    public void setUp(String browser) {
-        switch (browser.toLowerCase()) {
-            case "chrome":
-                driver = new ChromeDriver();
-                break;
-            case "edge":
-                driver = new EdgeDriver();
-                break;
-        }
-    }
+	@BeforeClass
+	@Parameters("browser")
+	public void setUpBrowser(String browser) {
+		switch (browser.toLowerCase()) {
+		case "chrome":
+			driver = new ChromeDriver();
+			bName = "Chrome";
+			break;
+		case "edge":
+			driver = new EdgeDriver();
+			bName = "Edge";
+			break;
+		}
+	}
 
-    @Test
-    public void testExample() {
-        driver.get("https://www.amazon.in/");
-        // Add your test logic here
-        System.out.println("Page title is: " + driver.getTitle());
-    }
-    
-    @AfterTest
-    public void afterTest() {
-    	
-    	
-    	
-    }
-    
-    
+	@Test
+	public void testExample() {
+		driver.get("https://www.amazon.in/");
+		// Add your test logic here
+		System.out.println("Page title is: " + driver.getTitle());
+	}
 
-    @AfterClass
-    public void tearDown() {
-        if (driver != null) {
-            driver.quit();
-        }
-    }
+	@AfterMethod
+	public void AfterMethod() throws IOException {
+
+		File file = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+
+		org.openqa.selenium.io.FileHandler.copy(file, new File(bName + "_ScreenShot" + ".jpeg"));
+		
+
+	}
+
+	@AfterClass
+	public void Quit() {
+		if (driver != null) {
+			driver.quit();
+		}
+	}
 }
